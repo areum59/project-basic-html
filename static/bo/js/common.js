39228601c06fa4c1ae7879936a168d07e1952f05
areum <areum59@soleo.kr>
2024-03-25 09:41:50 +0900

    BO header : 즐겨찾기 toggle 추가

---
+++ static/bo/js/common.js
@@ -0,0 +1,16 @@
+$(function(){
+    /** (참고) 
+     * JS inckyde시 script 미작동 오류로 인해
+     * body에서 이벤트를 발생시켜 자식요소에 이벤트 전달 
+     * 따라서 실제 프로젝트 진행시 선택자 수정 必
+     */
+
+    /** 
+	 * 아코디언(accordion) 기능
+     * 공통 script
+	 */
+     $("body").on("click", ".accordion-title", function () {
+		$(this).toggleClass('toggle');
+		$(this).next().slideToggle('300');
+	});
+});
